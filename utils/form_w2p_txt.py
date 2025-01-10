266b641ad1847d271741f0096244de8005fc6189
driver001 <andres.udo.info@gmail.com>
2025-01-10 19:31:11 -0400

    cambios

--- utils/form_w2p_txt.py
+++ utils/form_w2p_txt.py
@@ -239,7 +239,7 @@ def RTtxt(count,acum_total_20,acum_total_21,acum_total_22,acum_total_23,acum_tot
     acum_total_24 = round(acum_total_24 * 100)
     acum_total_24 = f"{acum_total_24:015d}"
       
-    return "RT"+count+add_to_right(text,30,"0")+acum_total_20+acum_total_21+acum_total_22+acum_total_23+acum_total_24+add_to_right(text,180,"0")+add_to_right(text,15,"0")+add_to_right(text,90,"0")+count+add_to_right(text,98," ")
+    return "RT"+count+add_to_right(text,30,"0")+acum_total_20+acum_total_21+acum_total_22+acum_total_23+acum_total_24+add_to_right(text,180,"0")+add_to_right(text,15,"0")+add_to_right(text,90,"0")+add_to_right(text,90,"15")+count+add_to_right(text,98," ")
 
 def RUtxt(count,acum_total_7,acum_total_8,acum_total_9,acum_total_10,acum_total_11,acum_total_13,acum_total_14):
     text = ""  
@@ -271,7 +271,7 @@ def RVtxt(count,company,acum_total_16,acum_total_6):
     acum_total_6 = round(acum_total_6 * 100)
     acum_total_6 = f"{acum_total_6:015d}"
       
-    return "RV"+company.contact_number.replace("-","").ljust(10)+add_to_right(text,20," ")+add_to_right(text,15,"0")+add_to_right(text,15,"0")+acum_total_16+add_to_right(text,15,"0")+acum_total_6+add_to_right(text,15,"0")+add_to_right(text,375," ")
+    return "RV"+company.contact_number.replace("-","").ljust(10)+add_to_right(text,20," ")+add_to_right(text,15,"0")+add_to_right(text,15,"0")+acum_total_16+add_to_right(text,15,"0")+acum_total_6+add_to_right(text,15,"0")+add_to_right(text,15,"0")+add_to_right(text,375," ")
 
 def RFtxt(count):
     text = ""  
